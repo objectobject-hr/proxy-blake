283eda0f399d6016a9ab8f59d2ecd355e077387c
Blake Mason <blakelmason@gmail.com>
2019-12-14 10:57:35 -0800

    updating server for deployment

--- index.js
+++ index.js
@@ -1,31 +1,38 @@
 const express = require('express')
-const proxy = require('http-proxy-middleware')
+// const proxy = require('http-proxy-middleware')
 const path = require('path')
+const controllerD = require('../David-service/server/controllers')
+const controllerB = require('../service-blake/db/')
+const controllerA = require('../service-aaron/server/controllers')
 
 const app = express()
 const port = 3000
 
 app.use(express.static(path.join(__dirname, 'dist')))
 
-const servers = [
-  { route: '/reviews', location: 'http://localhost:3003/reviews' },
-  { route: '/ikea', location: 'http://localhost:3007/ikea' },
-  { route: '/products', location: 'http://localhost:3001/products' }
-]
+// const servers = [
+//   { route: '/reviews', location: 'http://localhost:3003/reviews' },
+//   { route: '/ikea', location: 'http://localhost:3007/ikea' },
+//   { route: '/products', location: 'http://localhost:3001/products' }
+// ]
 
-for (server of servers) {
-  app.use(
-    server.route,
-    proxy({
-      target: server.location,
-      pathRewrite: (path, req) => {
-        return path
-          .split('/')
-          .slice(2)
-          .join('/')
-      }
-    })
-  )
-}
+// for (server of servers) {
+//   app.use(
+//     server.route,
+//     proxy({
+//       target: server.location,
+//       pathRewrite: (path, req) => {
+//         return path
+//           .split('/')
+//           .slice(2)
+//           .join('/')
+//       }
+//     })
+//   )
+// }
+
+app.get('/ikea', (req, res) => controllerD.getAll(req, res))
+app.get('/ikeaproducts', (req, res) => controllerA.getAll(req, res))
+app.get('/reviews', (req, res) => controllerB.get(req, res))
 
 app.listen(port, () => console.log(`listening on port ${port}`))
